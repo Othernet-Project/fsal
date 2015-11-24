8002ea79173cd7b6eff59c0553823c308f9a7022
Manish Mathai <manishpmathai@gmail.com>
2015-11-24 23:46:47 +0530

    Update migration scripts

--- fsal/migrations/fs/03_00_add_events_table.py
+++ fsal/migrations/fs/03_00_add_events_table.py
@@ -1,10 +1,10 @@
 SQL = """
 create table events
 (
-    id integer primary key not null,    -- id for event
+    id serial primary key not null,    -- id for event
     type varchar not null,              -- whether it's a create, modify or delete event
     src varchar not null,               -- path of the source of event
-    is_dir integer not null             -- whether the source is a directory
+    is_dir bool not null             -- whether the source is a directory
 );
 """
 
