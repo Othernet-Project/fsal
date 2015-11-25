c1fe2013e62d4a8185d39282d4975dc7ac63fb46
Manish Mathai <manishpmathai@gmail.com>
2015-11-26 00:06:49 +0530

    Use migrations from squery_pg

--- fsal/db/databases.py
+++ fsal/db/databases.py
@@ -8,8 +8,8 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-from .migrations import migrate
-from squery_pg import Database, DatabaseContainer
+from squery_pg.migrations import migrate
+from squery_pg.squery_pg import Database, DatabaseContainer
 
 
 def get_databases(db_name, host, port, user, password, debug=False):
