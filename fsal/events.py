55aecc798fcb88ef6685a192bf992aad8eff88c5
Manish Mathai <manishpmathai@gmail.com>
2015-12-01 23:29:49 +0530

    Correct count of cleared events in log

--- fsal/events.py
+++ fsal/events.py
@@ -150,4 +150,4 @@ class FileSystemEventQueue(object):
                 ids.append(row['id'])
             q = self.db.Delete(self.EVENTS_TABLE, where='id = %s')
             self.db.executemany(q, ((id,) for id in ids))
-            logging.debug('Cleared %d events' % num)
+            logging.debug('Cleared %d events' % len(ids))
