fd77f08269c6715d1d8e8171225f8f87d860d54c
Ben Ennis <ben@outernet.is>
2016-02-24 10:07:10 -0800

    a little OCD ordering

--- fsal/commandtypes.py
+++ fsal/commandtypes.py
@@ -10,16 +10,16 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
-COMMAND_TYPE_LIST_DIR = 'list_dir'
 COMMAND_TYPE_COPY = 'copy'
-COMMAND_TYPE_EXISTS = 'exists'
 COMMAND_TYPE_ISDIR = 'isdir'
+COMMAND_TYPE_EXISTS = 'exists'
 COMMAND_TYPE_ISFILE = 'isfile'
 COMMAND_TYPE_REMOVE = 'remove'
 COMMAND_TYPE_SEARCH = 'search'
 COMMAND_TYPE_REFRESH = 'refresh'
 COMMAND_TYPE_GET_FSO = 'get_fso'
 COMMAND_TYPE_TRANSFER = 'transfer'
+COMMAND_TYPE_LIST_DIR = 'list_dir'
 COMMAND_TYPE_GET_CHANGES = 'get_changes'
-COMMAND_TYPE_CONFIRM_CHANGES = 'confirm_changes'
 COMMAND_TYPE_REFRESH_PATH = 'refresh_path'
+COMMAND_TYPE_CONFIRM_CHANGES = 'confirm_changes'
