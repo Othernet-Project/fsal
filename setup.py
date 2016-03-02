e6168c29393c6aee6e4a9a68df3f02fb140de7a2
Andrean Franc <andrean.franc@gmail.com>
2016-03-02 13:48:21 +0100

    Add ondd-ipc to list of dependencies

--- setup.py
+++ setup.py
@@ -58,12 +58,14 @@ setup(
         'scandir>=0.9',
         'setuptools',
         'zippie',
+        'ondd_ipc',
         'librarian_core',
         'squery_pg',
     ],
     dependency_links=[
         'git+ssh://git@github.com/Outernet-Project/librarian-core.git#egg=librarian_core-0.1',
         'git+ssh://git@github.com/Outernet-Project/squery-pg.git#egg=squery_pg-0.1',
+        'git+ssh://git@github.com/Outernet-Project/ondd-ipc.git@develop#egg=ondd_ipc-0.1',
         'git+ssh://git@github.com/integricho/zippie.git#egg=zippie-0.0.1',
     ],
 )
